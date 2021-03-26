9c8591eeb6e517da07b9e4ff7b8c1da305f2d15d
William Angulo <wmavalle@gmail.com>
2021-03-26 00:59:34 -0500

    Checking documentation about this awesome language 😀 😆

--- Practising-week1.py
+++ Practising-week1.py
@@ -83,6 +83,105 @@ for x in "banana":
 
 a = "Hello, World"
 print(len(a))
-
+##checking strings
 txt = "William is handsome"
 print("handsome" in txt)
+#if not
+txt = "William is handsome"
+print("handsome" not in txt)
+##Slicing strings
+#with a range
+b = "William is the best "
+print(b[3:5])
+#since the start
+b = "QUE ONDA PA"
+print(b[:4])
+#to the end
+b="String is so hard"
+print(b[2:])
+#negative indexing
+b = "Hello, world!"
+print(b[-3:-1])
+##Modifying strings
+#upper
+a = "William wapo"
+print(a.upper())
+#lower
+a = "GAYS"
+print(a.lower())
+#strip
+#removes spaces from the beginning and also the end
+a = " Hello, GAY "
+print(a.strip())
+#replace
+a = "Hola gente de wsp"
+print(a.replace("H","w"))
+#split a comom function
+a = "Take away bro"
+print(a.split("y"))
+#we have to indicate the separation
+
+##String Concatenation
+##examples
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+##adding space
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+#formating strings
+age= 19
+txt = "My name is William, I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+#Escape characters
+#\'	Single Quote
+#\\	Backslash
+#\n	New Line
+#\r	Carriage Return
+#\t	Tab
+#\b	Backspace
+#\f	Form Feed
+#\ooo	Octal value
+#\xhh	Hex value
+
+##Booleans
+#bool
+print(bool("Hello"))
+print(bool(1))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+#Most values are true
+#any string are true except empty
+#We are going to see some values are false
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+class myclass():
+    def __len__(self):
+       return 1
+myobj = myclass()
+print(bool(myobj))
