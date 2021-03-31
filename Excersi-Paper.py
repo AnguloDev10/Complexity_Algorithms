159e9669cfbf273f54c1ff3145cd0ee25006c5cb
William <wmavalle@gmail.com>
2021-03-31 16:54:38 -0500

    jaywheeler

--- Excersi-Paper.py
+++ Excersi-Paper.py
@@ -35,3 +35,4 @@ stringmatching("rac", "abracadabracalamazoo")
 print(results)
 print(a)
 #Backtracking
+
--- Practising-week1-structure-tuple.py
+++ Practising-week1-structure-tuple.py
@@ -1,1 +1,22 @@
 ##Python Tuples
+thistuple = ("apple", "banana","cherry")
+print(thistuple[-1])
+
+#Rnage of index
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon")
+print(thistuple[2:4])
+
+#as well as the lists we hace a range of Negative indexes
+##SETS
+thisset = {"apple", "banana", "cherry"}
+tropical={"rata","gato","cherry"}
+
+thisset.update(tropical)
+print((thisset))
+#pop,discar()
+thisset.pop()
+#keep duplicates
+thisset = {"apple", "banana", "cherry"}
+tropical={"rata","gato","cherry"}
+thisset.intersection_update(tropical)
+print(thisset)
