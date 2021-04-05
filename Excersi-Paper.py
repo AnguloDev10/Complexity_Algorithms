2efc75da5192b49fe78671d7937ed1d077b84835
William Angulo <wmavalle@gmail.com>
2021-04-04 23:56:18 -0500

    git

--- Excersi-Paper.py
+++ Excersi-Paper.py
@@ -1,11 +1,11 @@
 #First-paper
 import random as ra
 
-a = []
-def generar(n):
-    b = []
-    for x in range(n):
-        a.append(int(ra.randint(10,100)))
+#a = []
+#def generar(n):
+#b = []
+#for x in range(n):
+#a.append(int(ra.randint(10,100)))
 
 
 
