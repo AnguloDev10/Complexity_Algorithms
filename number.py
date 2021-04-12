b52fb85c1e95acbe1555cf0f66a35c4e068fcf0a
William <wmavalle@gmail.com>
2021-04-11 19:03:25 -0500

    "leetcodeplss"

---
+++ number.py
@@ -0,0 +1,33 @@
+digits=input()
+m= len(digits)
+ara={}
+def castear():
+    if(n==0):
+        return []
+    if(n==1):
+        [()]
+    if(n==2):
+    if(n==3):
+    if(n==4):
+
+
+def valid():
+    ara[2]=['a','b','c']
+    ara[3]=['d','e','f']
+    ara[4]=['d','e','f']
+    ara[5]=['d','e','f']
+    ara[6]=['d','e','f']
+    ara[7]=['d','e','f']
+    ara[8]=['d','e','f']
+    ara[9]=['d','e','f']
+    comnbs=[]
+    #if len(digits) < 0:
+    #    return []
+    for c in range(len(a)):
+        for b in range(len(a)):
+            if c != b or c==b :
+                comnbs.append((a[c],x[b]))
+    
+    return comnbs
+
+print(valid())
